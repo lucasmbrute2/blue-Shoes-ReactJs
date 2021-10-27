ab93d1989ec14bec6cbe849367284696e31b77ea
viniflorencio <viniflorenciomid@gmail.com>
2021-10-27 17:49:28 -0300

    estruturaçao da arquitetura components

---
+++ src/components/ProdutoCard/ProdutoCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./ProdutoCard.css"; 
+import { Link } from "react-router-dom";
+
+export default function ProdutoCard() {
+    return (
+        <div>
+            ProdutoCard
+        </div>
+    )
+}
