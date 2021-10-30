c323d0143dff4f2c37123a8cf7835eb6fd98c824
Lucas <lucasmbrute614@gmail.com>
2021-10-30 13:11:52 -0300

    BSP- fix: setting new file structure in Login && text-align: center in Login h2

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -5,8 +5,27 @@ import SignIn from '../../SignIn/SignIn';
 
 export default function Login() {
     return (
-        <div>
-            <SignIn/>
+        <div className='form-container'>
+            <form className='form'>
+                <h2 className='form-h2'>Entrar</h2>
+                <div className='form-div'>
+                    <input className='form-div-input' placeholder=' '></input>
+                    <label className='form-div-label'>Email</label>
+                </div>
+            
+                <div className='form-div'>
+                    <input className='form-div-input' placeholder=' ' autoComplete={false}></input>
+                    <label className='form-div-label'>Senha</label>
+                </div>
+                <div>
+                    <input type='submit' className='form-button' autoComplete={false}></input>
+                </div>
+               
+                <div className='form-div-links'>
+                    <p className='form-div-links-p'>Não tem uma conta?<Link to='/register' className='form-button-links-link'>Cadastre aqui</Link></p>
+                    <p className='form-div-links-p'>Lembrar minha senha</p>
+                </div>
+            </form>
         </div>
     )
 }
