1bef5e9c3bf9cad58447b19a1785898194c8e0a9
Lucas <lucasmbrute614@gmail.com>
2021-10-29 22:53:47 -0300

    BSP-28 feat: page login created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Header/Navbar";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer"
+import Login from "./components/Pages/Login/Login";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       {/* <Switch>
         <Route path="/" exact={true} component={Home} />
       </Switch> */}
-     <Footer/>
+     <Route path='/login' component={Login}/>
     </Router>
   );
 }
