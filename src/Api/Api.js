86c207969e3e5fba7739091c82671f9e667fae67
Lucas <lucasmbrute614@gmail.com>
2021-11-01 20:25:15 -0300

    BSP-43 feat: created Api file with API call GET, POST, PUT, DELETE

---
+++ src/Api/Api.js
@@ -0,0 +1,44 @@
+export const Api = {
+    baseUrl = "https://blueshoes-api.herokuapp.com/",
+
+    authHeader: ()=>({
+        Authorization : "Bearer "+ localStorage.getItem('JWT')
+    }),
+
+    getAll: (path,auth)=>fetch(Api.baseUrl+path,{
+        method: "GET",
+        headers: auth? new Headers(Api.authHeader()): undefined
+    }),
+
+    getById: (path,id,auth)=> fetch(`${Api.baseUrl}${path}/${id}`,{
+        method :'GET',
+        headers: auth? new Headers(Api.authHeader()): undefined
+    }),
+    
+    post: (path,body,auth) => fetch(Api.baseUrl+path,{
+        method: "POST",
+        headers: new Headers({
+            "Content-type": "application/json",
+            ...(auth? Api.authHeader():{})
+        }),
+        body: JSON.stringify(body)
+    }),
+    
+    update: (path,body,id,auth)=>{
+        return fetch(Api.baseUrl+path+id,{
+            method: "PUT",
+            headers: new Headers({
+                "Content-type": "application/json",
+                ...(auth? Api.authHeader():{})
+            }),
+            body: JSON.stringify(body)
+        })
+    },
+
+    deletE: (path,id,auth)=>{
+        return fetch (Api.baseUrl+path+id,{
+            method: "DELETE",
+            headers: auth? new Headers(Api.authHeader()): undefined
+        })
+    }
+}
