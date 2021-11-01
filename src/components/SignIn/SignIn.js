429934488f6527fd9a56aa304b3ec41554b48543
Lucas <lucasmbrute614@gmail.com>
2021-11-01 15:59:43 -0300

    Revert "ajustes feito tela login, aguardando aprove"
    This reverts commit b9966e17485c6b40ca39032b009bf88cfc38b6c1.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,52 +1,27 @@
-import { Link } from "react-router-dom";
-import "./SignIn.css";
-import offV from "../assets/off_visibility_icon.png";
-import onV from "../assets/visibility_icon.png";
-import { useState } from "react";
-
-export default function SignIn() {
-  const [botao, setBotao] = useState(false);
-
-  function handleClick() {
-    const novoValor = botao ? false : true;
-    setBotao(novoValor);
-  }
-  return (
-    <div className="form-container">
-      <form className="form">
-        <h1 className="form-h1">Entrar</h1>
-        <div className="form-div">
-          <input className="form-div-input" placeholder=" "></input>
-          <label className="form-div-label">Email</label>
-        </div>
-        <div className="form-div">
-          <input
-            className="form-div-input"
-            type={botao ? 'text' : 'password'}
-            placeholder=""
-            autoComplete={false}
-          ></input>
-          <label className="form-div-label">Senha</label>
-          <span onClick={handleClick}>
-            <img className="form__img" src={botao ? offV : onV} alt="off" />
-          </span>
-        </div>
-        <div>
-          <input
-            type="submit"
-            className="form-button"
-            autoComplete={false}
-          ></input>
-        </div>
-        <div className="form-div-links">
-          <p className="form-div-links-p underline">Esqueceu sua senha?</p>
-          <p className="form-div-links-p">
-            <Link to="/register" className="form-button-links-link">
-              Não está Cadastrado ainda?
-            </Link>
-          </p>
-        </div>
-      </form>
+import { Link } from "react-router-dom"
+import "./SignIn.css"
+export default function SignIn(){
+    return(
+    <div className='form-container'>
+        <form className='form'>
+            <h1 className='form-h1'>Entrar</h1>
+            <div className='form-div'>
+                <input className='form-div-input' placeholder=' '></input>
+                <label className='form-div-label'>Email</label>
+            </div>
+        
+            <div className='form-div'>
+                <input className='form-div-input' placeholder=' ' autoComplete={false}></input>
+                <label className='form-div-label'>Senha</label>
+            </div>
+            <div>
+                <input type='submit' className='form-button' autoComplete={false}></input>
+            </div>
+            <div className='form-div-links'>
+                <p className='form-div-links-p'>Não tem uma conta?<Link to='/register' className='form-button-links-link'>Cadastre aqui</Link></p>
+                <p className='form-div-links-p'>Lembrar minha senha</p>
+            </div>
+        </form>
     </div>
-  );
+    )
 }
