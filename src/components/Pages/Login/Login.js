35c7ef013064083843ada11fbd2d204b4c36f4d0
menezesfelipee <sa.felipem@gmail.com>
2021-11-01 19:26:56 -0300

    BSP-42 fix importing

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Login.css"; 
 import { Link } from "react-router-dom";
-import SignIn from '../../SignIn/SignIn';
 
 export default function Login() {
     return (
