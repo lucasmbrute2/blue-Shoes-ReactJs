d8b742c2ef93c3bdb53d75c897cd3b1173cd3f0e
viniflorencio <viniflorenciomid@gmail.com>
2021-11-05 22:55:52 -0300

    BSP-30 Feat: listagem produtos pra home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Footer from "../../Footer/Footer"
 import Navbar from "../../Header/Navbar";
+import { ProductList } from "../../ProductList/ProductList";
+
 
 export default function Home() {
   return(
     <>
       <Navbar/>
-      <div>Home</div>
+      <div><ProductList/></div>
       <Footer/>
     </>
   ) 
