1398c2dd942945e78d73e8b0a74b74700649a4a4
Lucas <lucasmbrute614@gmail.com>
2021-11-06 16:51:32 -0300

    BSP-60: feat: Section with Brands

--- src/components/Brands/Brands.js
+++ src/components/Brands/Brands.js
@@ -1,17 +1,21 @@
 import "./Brands.css"
-import Nike from "../assets/nike.png"
+import Swoosh from "../assets/swoosh-medium.png"
+import Puma from "../assets/puma.png"
+import Adidas from "../assets/adidas.png"
 
 export default function Brands(){
     return(
         <div className='brands'>
-            <div>
-                <img src={Nike}/>
-            </div>
-            <div>
-                <img/>
-            </div>
-            <div>
-                <img/>
+            <div className='brands'>
+                <div>
+                    <img src={Swoosh} className='logo-img'/>
+                </div>
+                <div>
+                    <img src={Puma} className='logo-img'/>
+                </div>
+                <div>
+                    <img src={Adidas} className='logo-img'/>
+                </div>
             </div>
         </div>
     )
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import "./Home.css";
 import Footer from "../../Footer/Footer"
 import { ProductList } from "../../ProductList/ProductList";
 import Header from "../../Header/Header";
+import Brands from "../../Brands/Brands";
 
 export default function Home() {
   return(
     <>
       <Header/>
         <ProductList/>
+        <Brands/>
         <div>Home</div>
       <Footer/>
     </>
