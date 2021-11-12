46796f02b93170e4307675a627be72eabee94ab4
Thiago <thiago.ralves02@gmail.com>
2021-11-11 21:51:07 -0300

    feat: MyAccountEdit deleted and data updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MyAccount from "./components/Pages/My-Account/MyAccount";
-import MyAccountEdit from "./components/Pages/My-Account/MyAccountEdit";
 import Home from "./components/Pages/Home/Home";
 import Login from "./components/Pages/Login/Login";
 import Register from "./components/Pages/Register/Register";
 import ViewProduct from "./components/Pages/ViewProduct/ViewProduct";
-import ProductCard from "./components/ProductCard/ProductCard";
 import Cart from "./components/Pages/Cart/Cart";
 import Header from "./components/Header/Header";
 import Products from "./components/Pages/Products/Products";
@@ -19,7 +17,6 @@ function App() {
       <Switch>
         <Route path="/" exact={true} component = {Home}/>
         <Route path="/my-account" component={MyAccount} />
-        <Route path="/my-account-edit" component={MyAccountEdit} />
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
         <Route path='/view' component={ViewProduct}/>
