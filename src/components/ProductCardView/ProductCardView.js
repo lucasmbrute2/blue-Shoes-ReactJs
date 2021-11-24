da70ae0f3dbef7301aeb058a0490f5815be894b2
lucasmbrute2 <lucasmbrute614@gmail.com>
2021-11-23 23:12:16 -0300

    BSP-119 fix: new handleAddToCart function

--- src/components/ProductCardView/ProductCardView.js
+++ src/components/ProductCardView/ProductCardView.js
@@ -7,57 +7,66 @@ import { useHistory } from "react-router";
 export default function ProductCardView({ product }) {
     
     const history = useHistory()
-    const { setCartLocal } = useContext(Context)
-    
-    const HandleAddToCart = () =>{
+    const { cartLocal, setCartLocal } = useContext(Context)
+    let size = 39
+    const HandleAddToCart = (e) =>{
         
-        setCartLocal(product.id)
+        e.preventDefault()  
+
+        setCartLocal([...cartLocal,{
+            produto: product.nome,
+            tamanho: size,
+            id: product.id,
+            quantidade : 1
+        }])
+        
         alert("Produto adicionado ao carrinho")  
         history.push("/cart")
     }
     
     return (
         <div className="products">
-            <div className="product">
-                <div className="leftContent">
-                    <div className="card__image">
-                        <img className='card-img' src={product.imagem} alt={product.nome}></img>
+            <form onSubmit={(e)=>HandleAddToCart(e)}>            
+                <div className="product">
+                    <div className="leftContent">
+                        <div className="card__image">
+                            <img className='card-img' src={product.imagem} alt={product.nome}></img>
+                        </div>
                     </div>
-                </div>
-                <div className="rightContent">
+                    <div className="rightContent">
 
-                    <div className="card__title">
-                        <h1 className='card-title-h1'>{product.nome}</h1>
-                    </div>
-                    
-                    <div className="card__price">
-                        <p className="card__price__price">{product.preco.toFixed(2)}</p>
-                    </div>
+                        <div className="card__title">
+                            <h1 className='card-title-h1'>{product.nome}</h1>
+                        </div>
+                        
+                        <div className="card__price">
+                            <p className="card__price__price">{product.preco.toFixed(2)}</p>
+                        </div>
 
-                    <div className="card__color">
-                        <p className="card__color__color">  Escolha a cor desejada:</p>
-                        <ul className="color_options">
-                            <li>
-                                <button className={`color_choice `} type="button" ></button>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="card__size">
-                        <p className='card-size-p'> Escolha o tamanho desejado:</p>
-                            <ul className="size_options"> 
-                                {product.tamanhos.Branco.map(eachSize =>(
-                                    <li>
-                                        <button className="size_choice" type="button">{eachSize}</button>
-                                    </li>
-                                ))}
-
+                        <div className="card__color">
+                            <p className="card__color__color">  Escolha a cor desejada:</p>
+                            <ul className="color_options">
+                                <li>
+                                    <button className={`color_choice `} type="button" ></button>
+                                </li>
                             </ul>
-                    </div>
-                    <SubmitButton id="submitProductView" value="Adicionar ao carrinho" onClick={HandleAddToCart}/>                                        
-                </div>
+                        </div>
 
-            </div>
+                        <div className="card__size">
+                            <p className='card-size-p'> Escolha o tamanho desejado:</p>
+                                <ul className="size_options"> 
+                                <select className='size_select'>
+                                    {product.tamanhos.Branco.map(eachSize =>(
+                                            <option className="size_choice" type="button" onClick={size=eachSize}>{eachSize}</option>
+                                            ))}
+                                </select>
+
+                                </ul>
+                        </div>
+                        <SubmitButton id="submitProductView" value="Adicionar ao carrinho"/>                                        
+                    </div>
+                </div>
+            </form>
 
         </div>
     )
