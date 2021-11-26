a37204d69ff5d791131f3bc3636e3eeef633254f
Lucas <lucasmbrute614@gmail.com>
2021-11-26 14:05:45 -0300

    BSP-142 fix: removed useHistory from SideFilterBrands

--- src/components/SideFilterBrands/SideFilterBrands.js
+++ src/components/SideFilterBrands/SideFilterBrands.js
@@ -4,19 +4,18 @@ import { Api } from '../../Api/Api'
 import { Context } from '../../context/CtxApp'
 
 export default function SideFilterBrands({brand}) {
-    const history = useHistory()
-    const { user, setProduct } = useContext(Context)
+    
+    const { setProduct } = useContext(Context)
     const handleClick = async (e)=> {
         e.preventDefault()
         
         const response = await Api.getAll(`produto?marca=${brand.nome}`)
         const body = await response.json()
         setProduct(body)
-        history.push('/products')
     }
     return (
         <div>
-            <p  onClick={handleClick}>{brand.nome}</p>
+            <p onClick={handleClick}>{brand.nome}</p>
         </div>
     )
 }
